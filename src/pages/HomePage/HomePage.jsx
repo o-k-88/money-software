947599579e9142781ed0038f4cc2e7cf21da98b5
Oleh Kyrylenko <oleh.kyrylenko@sunyempire.edu>
2025-03-26 12:00:25 -0400

    update 3/26/2025

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -22,7 +22,7 @@ import andreiImg from "./images/andrei-kirilenko.jpg";
 
 import { REVIEWS } from "../../helpers/feedbacks";
 import CodeBlock from "../../components/CodeBlock/CodeBlock";
-import line1 from "./images/line1.svg";
+import line from "./images/line.svg";
 
 const Homepage = () => {
   return (
@@ -73,7 +73,7 @@ const Homepage = () => {
             <CodeBlock />
           </Container>
           <Box sx={{ position: "absolute", right: "50%", bottom: "-48px" }}>
-            <Box component="img" src={line1} />
+            <Box component="img" src={line} />
           </Box>
         </Container>
       </Box>
@@ -95,7 +95,7 @@ const Homepage = () => {
           </Box>
 
           <Box sx={{ textAlign: "center" }}>
-            <Box component="img" src={line1} sx={{ stroke: "red" }} />
+            <Box component="img" src={line} sx={{ stroke: "red" }} />
           </Box>
 
           <Accordion sx={{ bgcolor: "grey.100", mb: 2 }}>
